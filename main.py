8a4908cc5743fd7c3ede92b8d82d88ffdfb025a0
vin267 <vineet27091991@gmail.com>
2021-05-17 22:17:03 -0400

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import re
 
 page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -28,7 +29,19 @@ descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
 weather = pd.DataFrame({
     "periods": periods,
     "short_descs": short_descs,
-    "temps": temps,
+    "temp": temps,
     "descs": descs
 })
-print(weather["temps"])
+#temp_nums = weather["temp"].str.extract("(?Pd+)", expand=False)
+temp_nums = weather["temp"].str.extract("(?P<num>\d+)", expand=False)
+weather["temp_num"] = temp_nums.astype('int')
+#print(weather["temp_num"])
+
+#mean
+#print(weather["temp_num"].mean())
+
+#select rows that happen at night
+is_night = weather["temp"].str.contains("Low")
+weather["is_night"] = is_night
+print(is_night)
+
